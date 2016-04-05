e922d99125cd3c66c25f12c019c339fc31d4fe31
Michael Davidsaver <mdavidsaver@gmail.com>
2016-04-04 20:52:57 -0400

    Imported Upstream version 6.9

---
+++ motorApp/AerotechSrc/AerotechRegister.cc
@@ -0,0 +1,129 @@
+/*
+FILENAME...	AerotechRegister.cc
+USAGE...	Register Aerotech motor device driver shell commands.
+
+Version:        $Revision: 17434 $
+Modified By:    $Author: sluiter $
+Last Modified:  $Date: 2014-05-21 11:43:51 -0500 (Wed, 21 May 2014) $
+HeadURL:        $URL: https://subversion.xray.aps.anl.gov/synApps/motor/tags/R6-9/motorApp/AerotechSrc/AerotechRegister.cc $
+*/
+
+/*****************************************************************
+                          COPYRIGHT NOTIFICATION
+*****************************************************************
+
+(C)  COPYRIGHT 1993 UNIVERSITY OF CHICAGO
+
+This software was developed under a United States Government license
+described on the COPYRIGHT_UniversityOfChicago file included as part
+of this distribution.
+**********************************************************************/
+
+#include <iocsh.h>
+#include "AerotechRegister.h"
+#include "epicsExport.h"
+
+extern "C"
+{
+
+// Aerotech non-asyn Setup arguments
+static const iocshArg setupArg0 = {"Max. controller count", iocshArgInt};
+static const iocshArg setupArg1 = {"Polling rate", iocshArgInt};
+// Aerotech non-asyn Config arguments
+static const iocshArg configArg0 = {"Card being configured", iocshArgInt};
+static const iocshArg configArg1 = {"asyn port name", iocshArgString};
+static const iocshArg configArg2 = {"asyn address", iocshArgInt};
+
+static const iocshArg * const SoloistSetupArgs[2]  = {&setupArg0, &setupArg1};
+static const iocshArg * const SoloistConfigArgs[3] = {&configArg0, &configArg1, &configArg2};
+
+static const iocshFuncDef setupSoloist  = {"SoloistSetup",  2, SoloistSetupArgs};
+static const iocshFuncDef configSoloist = {"SoloistConfig", 2, SoloistConfigArgs};
+
+// Ensemble Asyn Setup arguments
+static const iocshArg ensembleAsynSetupArg0 = {"Max. controller count", iocshArgInt};
+// Ensemble Asyn Config arguments
+static const iocshArg ensembleAsynConfigArg0 = {"Card being configured", iocshArgInt};
+static const iocshArg ensembleAsynConfigArg1 = {"asyn port name", iocshArgString};
+static const iocshArg ensembleAsynConfigArg2 = {"asyn address (GPIB)", iocshArgInt};
+static const iocshArg ensembleAsynConfigArg3 = {"Number of Axes", iocshArgInt};
+static const iocshArg ensembleAsynConfigArg4 = {"Moving poll rate", iocshArgInt};
+static const iocshArg ensembleAsynConfigArg5 = {"Idle poll rate", iocshArgInt};
+
+// A3200 Asyn Setup arguments
+static const iocshArg a3200AsynSetupArg0 = {"Max. controller count", iocshArgInt};
+// A3200 Asyn Config arguments
+static const iocshArg a3200AsynConfigArg0 = {"Card being configured", iocshArgInt};
+static const iocshArg a3200AsynConfigArg1 = {"asyn port name", iocshArgString};
+static const iocshArg a3200AsynConfigArg2 = {"asyn address", iocshArgInt};
+static const iocshArg a3200AsynConfigArg3 = {"Number of Axes", iocshArgInt};
+static const iocshArg a3200AsynConfigArg4 = {"Task number", iocshArgInt};
+static const iocshArg a3200AsynConfigArg5 = {"Moving poll rate", iocshArgInt};
+static const iocshArg a3200AsynConfigArg6 = {"Idle poll rate", iocshArgInt};
+
+static const iocshArg * const EnsembleAsynSetupArgs[2] = {&ensembleAsynSetupArg0};
+static const iocshArg * const EnsembleAsynConfigArgs[6] = {&ensembleAsynConfigArg0, 
+    &ensembleAsynConfigArg1,
+    &ensembleAsynConfigArg2,
+    &ensembleAsynConfigArg3,
+    &ensembleAsynConfigArg4,
+    &ensembleAsynConfigArg5};
+
+static const iocshFuncDef setupEnsembleAsyn = {"EnsembleAsynSetup", 1, EnsembleAsynSetupArgs};
+static const iocshFuncDef configEnsembleAsyn = {"EnsembleAsynConfig", 6, EnsembleAsynConfigArgs};
+
+static const iocshArg * const A3200AsynSetupArgs[2] = {&a3200AsynSetupArg0};
+static const iocshArg * const A3200AsynConfigArgs[7] = {&a3200AsynConfigArg0, 
+    &a3200AsynConfigArg1,
+    &a3200AsynConfigArg2,
+    &a3200AsynConfigArg3,
+    &a3200AsynConfigArg4,
+    &a3200AsynConfigArg5,
+    &a3200AsynConfigArg6};
+
+static const iocshFuncDef setupA3200Asyn = {"A3200AsynSetup", 1, A3200AsynSetupArgs};
+static const iocshFuncDef configA3200Asyn = {"A3200AsynConfig", 7, A3200AsynConfigArgs};
+
+static void setupSoloistCallFunc(const iocshArgBuf *args)
+{
+    SoloistSetup(args[0].ival, args[1].ival);
+}
+static void configSoloistCallFunc(const iocshArgBuf *args)
+{
+    SoloistConfig(args[0].ival, args[1].sval, args[2].ival);
+}
+
+static void setupEnsembleAsynCallFunc(const iocshArgBuf *args)
+{
+    EnsembleAsynSetup(args[0].ival);
+}
+static void configEnsembleAsynCallFunc(const iocshArgBuf *args)
+{
+    EnsembleAsynConfig(args[0].ival, args[1].sval, args[2].ival, 
+                       args[3].ival, args[4].ival, args[5].ival);
+}
+
+static void setupA3200AsynCallFunc(const iocshArgBuf *args)
+{
+    A3200AsynSetup(args[0].ival);
+}
+static void configA3200AsynCallFunc(const iocshArgBuf *args)
+{
+    A3200AsynConfig(args[0].ival, args[1].sval, args[2].ival, 
+                       args[3].ival, args[4].ival, args[5].ival, args[6].ival);
+}
+
+static void AerotechRegister(void)
+{
+    iocshRegister(&setupSoloist, setupSoloistCallFunc);
+    iocshRegister(&configSoloist, configSoloistCallFunc);
+    iocshRegister(&setupEnsembleAsyn, setupEnsembleAsynCallFunc);
+    iocshRegister(&configEnsembleAsyn, configEnsembleAsynCallFunc);
+    iocshRegister(&setupA3200Asyn, setupA3200AsynCallFunc);
+    iocshRegister(&configA3200Asyn, configA3200AsynCallFunc);
+}
+
+epicsExportRegistrar(AerotechRegister);
+
+} // extern "C"
+
---
+++ motorApp/PIGCS2Src/PIGCSPiezoController.h
@@ -0,0 +1,51 @@
+/*
+FILENAME...     PIGCScontroller.h
+
+*************************************************************************
+* Copyright (c) 2011-2013 Physik Instrumente (PI) GmbH & Co. KG
+* This file is distributed subject to the EPICS Open License Agreement
+* found in the file LICENSE that is included with this distribution.
+*************************************************************************
+
+Version:        $Revision: 3$
+Modified By:    $Author: Steffen Rau$
+Last Modified:  $Date: 25.10.2013 10:43:08$
+HeadURL:        $URL$
+
+Original Author: Steffen Rau 
+Created: 15.12.2010
+*/
+
+#ifndef PIGCSPIEZOCONTROLLER_H_
+#define PIGCSPIEZOCONTROLLER_H_
+
+#include "PIGCSController.h"
+#include <asynDriver.h>
+
+/**
+ * Class representing PI's GCS2 digital piezo controllers.
+ *
+ * Main difference to motor controllers is the usage of absolute sensors.
+ */
+class PIGCSPiezoController : public PIGCSController
+{
+public:
+	PIGCSPiezoController(PIInterface* pInterface, const char* szIDN)
+	: PIGCSController(pInterface, szIDN)
+	{
+	}
+	~PIGCSPiezoController() {}
+
+	virtual asynStatus init(void) { return PIGCSController::init(); }
+	virtual asynStatus initAxis(PIasynAxis* pAxis);
+	virtual asynStatus haltAxis(PIasynAxis* pAxis);
+
+    virtual asynStatus getStatus(PIasynAxis* pAxis, int& homing, int& moving, int& negLimit, int& posLimit, int& servoControl);
+    virtual asynStatus getReferencedState(PIasynAxis* pAxis);
+
+
+private:
+
+};
+
+#endif /* PIGCSPIEZOCONTROLLER_H_ */
